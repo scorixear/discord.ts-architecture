8eb88801fc401aeeb8d129bf20d10a6767a89c36
Paul Keller <contact.keller.paul@gmail.com>
2024-04-19 21:17:17 +0200

    added selectionmenuinteraction to export

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,13 @@ import { Logger, WARNINGLEVEL } from './helpers/logging';
 import { AutocompleteInteractionModel } from './model/AutocompleteInteractionModel';
 import { ButtonInteractionModel } from './model/ButtonInteractionModel';
 import { CommandInteractionModel } from './model/CommandInteractionModel';
+import { SelectMenuInteractionModel } from './model/SelectMenuInteractionModel';
+import { AnySelectMenuInteractionModel } from './model/SelectMenuInteractionModels/AnySelectionMenuInteractionModel';
+import { ChannelSelectMenuInteractionModel } from './model/SelectMenuInteractionModels/ChannelSelectionMenuInteractionModel';
+import { MentionableSelectMenuInteractionModel } from './model/SelectMenuInteractionModels/MentionableSelectMenuInteractionModel';
+import { RoleSelectMenuInteractionModel } from './model/SelectMenuInteractionModels/RoleSelectionMenuInteractionModel';
+import { StringSelectMenuInteractionModel } from './model/SelectMenuInteractionModels/StringSelectMenuInteractionModel';
+import { UserSelectMenuInteractionModel } from './model/SelectMenuInteractionModels/UserSelectMenuInteractionModel';
 import { TwoWayMap } from './model/TwoWayMap';
 
 export {
@@ -18,5 +25,12 @@ export {
   AutocompleteInteractionModel,
   ButtonInteractionModel,
   CommandInteractionModel,
+  AnySelectMenuInteractionModel,
+  ChannelSelectMenuInteractionModel,
+  MentionableSelectMenuInteractionModel,
+  RoleSelectMenuInteractionModel,
+  StringSelectMenuInteractionModel,
+  UserSelectMenuInteractionModel,
+  SelectMenuInteractionModel,
   TwoWayMap
 };
